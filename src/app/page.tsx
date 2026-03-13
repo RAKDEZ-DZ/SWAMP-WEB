ea80de92b0b5d02f5d814b4320cff552f8ede693
raouf korichi <raoufbejaia2@gmail.com>
2026-03-13 05:09:22 +0100

    re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,15 @@
-
 'use client'
-
 import React, { useEffect, useState } from "react";
 import { Navbar, Footer } from "@/components";
 import VideoIntro from "@/components/video-intro";
 import Feature from "@/components/feature";
 import Faqs from "@/components/faqs";
-import { Loading3D } from "@/components/loading3D";
 import Hero from "@/components/hero";
 import Contact from "@/components/contact";
 import Carousel from "@/components/Carousel";
 import PricingSection from "@/components/pricings";
 
 export default function Campaign() {
-  const [showContent, setShowContent] = useState(false);
-  const [heroPreloaded, setHeroPreloaded] = useState(false);
-
-  useEffect(() => {
-    const handlePreloading = async () => {
-      const minLoadingTime = new Promise(resolve => 
-        setTimeout(resolve, 5000)
-      );
-
-      const preloadResources = new Promise<void>(resolve => {
-        const img = new Image();
-        img.onload = () => setHeroPreloaded(true);
-        img.onerror = () => setHeroPreloaded(true);
-        
-        setTimeout(() => {
-          setHeroPreloaded(true);
-          resolve();
-        }, 100);
-      });
-
-      await Promise.all([minLoadingTime, preloadResources]);
-      
-      setShowContent(true);
-    };
-    handlePreloading();
-  }, []);
-
-  if (!showContent) {
-    return (
-      <div>
-        <Loading3D />
-        <div style={{ 
-          position: 'absolute', 
-          left: '-9999px', 
-          visibility: 'hidden',
-          pointerEvents: 'none'
-        }}>
-          <Hero />
-        </div>
-      </div>
-    );
-  }
 
   return (
     <>
